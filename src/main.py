83a0e562779671edd0660a7315e378cb6174c345
Giesmundo <luc.redaelli@gmail.com>
2026-08-04 15:44:23 +0200

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import Clean_Clusters as Clean_Clusters
+import Merge_Clusters as Merge_Clusters
+import time
+from pathlib import Path
+import json
+
+"""
+i = r""
+o = r""
+I = Path(i)
+O = Path(o)
+with I.open(encoding="utf-8") as f:
+    clusters: list = json.load(f)
+
+cls: list = []
+for cluster in clusters:
+    cls.append(cluster.get("title"))
+
+with O.open("w", encoding="utf-8") as f:
+    json.dump(cls, f, ensure_ascii=False, indent=2)
+
+raise RuntimeError
+"""
+
+inizio = time.perf_counter()
+with open(r"D:\Tesi PY\errors\errori.txt", "w") as f:
+    f.write("")
+print("----------------Inizio main-----------------------")
+print("----------------INIZIO CLEAN_CLUSTERS-----------------")
+O_path: str = Clean_Clusters.run(r"D:\Tesi PY\data\Cluster_Da_Pullire.json", context = True, NER_type = True, batch_length = 10)
+print("----------------FINITO CLEAN_CLUSTERS-------------")
+print("----------------INIZIO MERGE_CLUSTERS-------------")
+O_path = Merge_Clusters.run(O_path, batch_to_be_combined = 2, batch_length = 50)
+print("----------------FINE MERGE_CLUSTERS---------------")
+fine = time.perf_counter()
+print(f"Tempo di esecuzione totale: {(fine - inizio)/60:.2f} minuti")
